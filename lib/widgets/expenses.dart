edbaeca4f4a3b88f138d2439307686aff9885af5
Avatbar <tadeasmachacek@gmail.com>
2023-04-24 00:09:19 +0200

    New_expense are stored now

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -31,8 +31,14 @@ class _ExpensesState extends State<Expenses> {
     showModalBottomSheet(
         context: context,
         // isScrollControlled: true,
-        builder: (ctx) => const NewExpense(),
+        builder: (ctx) => NewExpense(onAddExpense: _addExpense,),
     );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
   }
 
   @override
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
@@ -34,6 +36,40 @@ class _NewExpenseState extends State<NewExpense> {
     setState(() {
       _selectedDate = pickDate;
     });
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text("Invalid input"),
+            content: const Text("Please make sure that valid title, amount, date and category was entered"),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                  child: const Text("Okay"),),
+            ],
+          ),
+      );
+      return;
+    }
+
+    widget.onAddExpense(
+        Expense(
+            title: _titleController.text,
+            amount: enteredAmount,
+            date: _selectedDate!,
+            category: _selectedCategory
+        ),
+    );
   }
 
   @override
@@ -112,10 +148,7 @@ class _NewExpenseState extends State<NewExpense> {
                   },
                   child: const Text("Cancel")),
               ElevatedButton(
-                  onPressed: () {
-                    print(_titleController.text);
-                    print(_amountController.text);
-                  },
+                  onPressed: _submitExpenseData,
                   child: const Text("Save Expense"))
             ],
           )
