74c0790e28454315c806346d0776e08960ee99ac
Avatbar <tadeasmachacek@gmail.com>
2023-04-23 22:33:30 +0200

    added in to BottomSheet Amount field

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,19 +8,53 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-          children: const [
+          children: [
             TextField(
+              controller: _titleController,
               maxLength: 50,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 label: Text("Title")
               ),
             ),
-
+            TextField(
+              controller: _amountController,
+              keyboardType: const TextInputType.numberWithOptions(),
+              decoration: const InputDecoration(
+                prefixText: "€ ",
+                  label: Text("Amount"),
+              ),
+            ),
+            Row(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Cancel")
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print(_titleController.text);
+                      print(_amountController.text);
+                    },
+                    child: const Text("Save Expense")
+                )
+            ],)
           ],
         ),
     );
